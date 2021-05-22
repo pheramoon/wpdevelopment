141b5d401fdaa1663a8653bc2b208fba854d4784
Mehdi <mehdi@flowpress.com>
2021-05-22 16:51:24 -0400

    add header nav

--- wp-content/themes/mehdi-starter/functions.php
+++ wp-content/themes/mehdi-starter/functions.php
@@ -6,9 +6,12 @@
 
 // Includes
 include( get_theme_file_path( '/includes/front/enqueue.php' ) );
+include( get_theme_file_path( '/includes/setup.php' ) );
+
 
 // Hooks
 
-add_action('wp_enqueue_scripts', 'ju_enqueue');  
+add_action('wp_enqueue_scripts', 'ju_enqueue');
+add_action( 'after_setup_theme', 'ju_setup_theme' );
 
 // Shortcodes
