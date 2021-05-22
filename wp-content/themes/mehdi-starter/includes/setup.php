ffcb5b69b8621ab1f7b8c31d329ba359969d4028
Mehdi <mehdi@flowpress.com>
2021-05-22 18:01:28 -0400

    post thumbnails

--- wp-content/themes/mehdi-starter/includes/setup.php
+++ wp-content/themes/mehdi-starter/includes/setup.php
@@ -1,6 +1,6 @@
 <?php
 
 function ju_setup_theme() {
-    add_theme_support( 'post_thumbnails' );
+    add_theme_support( 'post-thumbnails' );
     register_nav_menu( 'primary', __('Primary Menu', 'mehdi-stater') );
 }
