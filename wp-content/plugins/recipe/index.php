0f91fb5b62e3328ed295b1a228b122095b83eb0a
Mehdi <mehdi@flowpress.com>
2021-05-24 12:08:40 -0400

    add meta data filter hook to the recipe custom post type

--- wp-content/plugins/recipe/index.php
+++ wp-content/plugins/recipe/index.php
@@ -18,10 +18,12 @@ if ( !function_exists ( 'add_action' ) ) {
 include( 'includes/activate.php' );
 include( 'includes/init.php' );
 include( 'process/save-post.php' );
+include( 'process/filter-content.php' );
 
 // Hooks
 register_activation_hook( __FILE__, 'r_activate_plugin' );
 add_action ( 'init', 'recipe_init' );
 add_action ( 'save_post_recipes', 'r_save_post_admin', 10, 3);
+add_filter( 'the_content', 'r_filter_recipe_content' );
 
 // Shortcodes
