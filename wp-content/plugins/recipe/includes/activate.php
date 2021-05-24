b90020bfb2efa081665683fa0a7a3317a9338448
Mehdi <mehdi@flowpress.com>
2021-05-24 13:29:47 -0400

    create database table via plugin activation

--- wp-content/plugins/recipe/includes/activate.php
+++ wp-content/plugins/recipe/includes/activate.php
@@ -5,4 +5,19 @@ function r_activate_plugin(){
     if ( version_compare( get_bloginfo ( 'version' ), '5.0', '<' ) ) {
         wp_die( __( "You must update wordpress to use this plugin", 'recipe' ) );
     }
+
+    global $wpdb;
+    $createSQL= 
+    "CREATE TABLE `" . $wpdb->prefix . "recipe_ratings` (
+        `ID` bigint(20) unsigned NOT NULL,
+        `recipe_id` bigint(20) unsigned DEFAULT NULL,
+        `rating` float unsigned DEFAULT NULL,
+        `user_ip` varchar(50) DEFAULT NULL,
+        PRIMARY KEY (`ID`),
+        UNIQUE KEY `ID_UNIQUE` (`ID`)
+    ) ENGINE=InnoDB " . $wpdb->get_charset_collate() . ";";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $createSQL );
+
 }
