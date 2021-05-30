e5d983d2370da04c7c9fcabe20e405b3b3c0fdea
Mehdi <mehdi@flowpress.com>
2021-05-30 10:55:42 -0400

    create widget and cron job

--- wp-content/plugins/recipe/index.php
+++ wp-content/plugins/recipe/index.php
@@ -18,15 +18,21 @@ define( 'RECIPE_PLUGIN_URL', __FILE__ );
 
 // Includes
 include( 'includes/activate.php' );
+include( 'includes/deactivate.php' );
 include( 'includes/init.php' );
 include( 'process/save-post.php' );
 include( 'process/filter-content.php' );
 include( 'includes/front/enqueue.php' );
 include( 'process/rate-recipe.php' );
 include( 'includes/admin/init.php' );
+include( dirname(RECIPE_PLUGIN_URL) . '/includes/widgets.php' );
+include( 'includes/widgets/daily-recipe.php' );
+include( 'includes/cron.php' );
+include( 'includes/utility.php' );
 
 // Hooks
 register_activation_hook( __FILE__, 'r_activate_plugin' );
+register_deactivation_hook( __FILE__, 'r_deactivate_plugin' );
 add_action( 'init', 'recipe_init' );
 add_action( 'save_post_recipe', 'r_save_post_admin', 10, 3 );
 add_filter( 'the_content', 'r_filter_recipe_content' );
@@ -34,5 +40,7 @@ add_action( 'wp_enqueue_scripts', 'r_enqueue_scripts', 100 );
 add_action( 'wp_ajax_r_rate_recipe', 'r_rate_recipe' );
 add_action( 'wp_ajax_nopriv_r_rate_recipe', 'r_rate_recipe' );
 add_action( 'admin_init', 'recipe_admin_init' );
+add_action( 'widgets_init', 'r_widgets_init' );
+add_action( 'r_daily_recipe_hook', 'r_daily_generate_recipe' );
 
 // Shortcodes
